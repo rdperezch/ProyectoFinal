936abd4ebfce168aafaf9843c990c135a3a012fb
Juan Gonzalez <juanbgnzlzm@gmail.com>
2016-04-17 10:00:19 -0500

    Nueva Interaz terminada BONITA perfecta

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
 from ConeccionDB import insertar, consultarlogin
@@ -12,48 +13,50 @@ class ScreenManagement(ScreenManager):
 class Intro(Screen):
     username_b = ObjectProperty()
     password_b = ObjectProperty()
+    background_image = ObjectProperty(Image(source='data/Campus_UIP.jpg'))
 
     def login(self):
         cerd = (self.username_b.text)
         passw = (self.password_b.text)
-        consultarlogin(cerd, passw)
+        # consultarlogin(cerd, passw)
 
 
 class Registro(Screen):
     nombre_b = ObjectProperty()
     apellido_b = ObjectProperty()
-    nacimiento_b = ObjectProperty()
+    seg_nombre_b = ObjectProperty()
     cedula_b = ObjectProperty()
-    carrera_b = ObjectProperty()
+    seg_apellido_b = ObjectProperty()
+    background_image = ObjectProperty(Image(source='data/blueback.png'))
 
     def regis(self):
         nombre = (self.nombre_b.text)
         apellido = (self.apellido_b.text)
-        nacimiento = (self.nacimiento_b.text)
-        cedula  = (self.cedula_b.text)
-        carrera = (self.carrera_b.text)
-        insertar(cedula, nombre, carrera, apellido, nacimiento)
+        seg_nombre = (self.seg_nombre_b.text)
+        cedula = (self.cedula_b.text)
+        seg_apellido = (self.seg_apellido_b.text)
+        # insertar(cedula, nombre, seg_nombre, apellido, seg_apellido)
 
 
 class Menu(Screen):
-    pass
+    background_image = ObjectProperty(Image(source='data/Build.jpg'))
 
 
 class Materias(Screen):
-    pass
+    grid = ObjectProperty()
+    background_image = ObjectProperty(Image(source='data/backazul.png'))
 
 
 class Calendario(Screen):
-    pass
+    background_image = ObjectProperty(Image(source='data/backazul.png'))
 
 
 class Perfil(Screen):
-    pass
+    background_image = ObjectProperty(Image(source='data/backazul.png'))
 
 
 class Eventos(Screen):
-    pass
-
+    background_image = ObjectProperty(Image(source='data/backazul.png'))
 
 kivyfile = Builder.load_file("Interfaz.kv")
 
