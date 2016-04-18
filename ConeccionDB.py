c506cff9d2c26832b95c28baee470b5d5ad6e490
Juan Gonzalez <juanbgnzlzm@gmail.com>
2016-04-18 18:59:55 -0500

    Comits final
    arreglando un poco de detallitos de interfaz
    Perfil Estudiantes Terminado
    Cambios en la coneccionDB

--- ConeccionDB.py
+++ ConeccionDB.py
@@ -2,7 +2,7 @@ import requests
 
 
 def insertar(cedula, pri_nombre, seg_nombre, pri_apellido, seg_apellido):
-    res = requests.post('http://192.168.173.1:5000/api/insertar_data/',
+    res = requests.post('http://localhost:5000/api/insertar_data/',
                         json={"cedula": cedula, "pri_nombre": pri_nombre,
                               "seg_nombre": seg_nombre, "pri_apellido": pri_apellido,
                               "seg_apellido": seg_apellido})
@@ -13,12 +13,38 @@ def insertar(cedula, pri_nombre, seg_nombre, pri_apellido, seg_apellido):
 
 
 def consultarlogin(cedula, password):
-    res = requests.post('http://192.168.173.1:5000/api/login/',
-                        json={"cedula": cedula, "password": password})
+    try:
+        res = requests.post('http://localhost:5000/api/login/',
+                            json={"cedula": cedula, "password": password})
+        global var
+        if res.ok:
+            var = True
+            print(res.json())
+        else:
+            var = False
+            print('Cedula no existente')
+    except Exception as e:
+        print("Error en base de datos")
+
+
+def conect():
+    return var
+
+
+def consultarperfil(cedula):
+    print('Consultar Data Request')
+    print('<--------------------------->')
+    res = requests.post('http://localhost:5000/api/consultar_perfil/', json={"cedula": cedula})
+    global perfil
     if res.ok:
-       print(res.json())
+        perfil = res.json()
     else:
         print('Cedula no existente')
+        print('--------------------------------------')
+
+
+def perfil():
+    return perfil
 
 # if __name__ == '__main__':
 #    insertar("8-359-545", "popo", "pepe", "manuel", "analinda")
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.image import Image
+from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import ObjectProperty
-from ConeccionDB import insertar, consultarlogin
+from ConeccionDB import insertar, consultarlogin, conect, consultarperfil, perfil
 
 
 class ScreenManagement(ScreenManager):
@@ -16,30 +17,69 @@ class Intro(Screen):
     background_image = ObjectProperty(Image(source='data/Campus_UIP.jpg'))
 
     def login(self):
-        cerd = (self.username_b.text)
-        passw = (self.password_b.text)
-        # consultarlogin(cerd, passw)
+        stat = self.ids.stats
+        try:
+            cerd = self.username_b.text
+            passw = self.password_b.text
+            consultarlogin(cerd, passw)
+            logic = conect()
+            if logic:
+                print("conecto")
+                stat.color = (0, 1, 0, 1)
+                stat.text = "Conecto"
+                self.manager.current = 'menu'
+                global manejador
+                manejador = cerd
+            if not logic:
+                stat.color = (1, 0, 0, 1)
+                stat.text = "Fallo"
+                print("no conecto")
+        except Exception as e:
+            stat.color = (1, 0, 0, 1)
+            stat.text = "Sin coneccion a internet"
+            print("Error en Login: sin coneccion", e)
+
+    def resetForm(self):
+        self.ids['username_i'].text = ""
+        self.ids['password_i'].text = ""
+
+    def resetLog(self):
+        self.ids['stats'].text = ""
 
 
 class Registro(Screen):
-    nombre_b = ObjectProperty()
-    apellido_b = ObjectProperty()
-    seg_nombre_b = ObjectProperty()
     cedula_b = ObjectProperty()
+    nombre_b = ObjectProperty()
+    seg_nombre_b = ObjectProperty()
+    apellido_b = ObjectProperty()
     seg_apellido_b = ObjectProperty()
     background_image = ObjectProperty(Image(source='data/blueback.png'))
 
     def regis(self):
-        nombre = (self.nombre_b.text)
-        apellido = (self.apellido_b.text)
-        seg_nombre = (self.seg_nombre_b.text)
-        cedula = (self.cedula_b.text)
-        seg_apellido = (self.seg_apellido_b.text)
-        # insertar(cedula, nombre, seg_nombre, apellido, seg_apellido)
+        nombre = self.nombre_b.text
+        apellido = self.apellido_b.text
+        seg_nombre = self.seg_nombre_b.text
+        cedula = self.cedula_b.text
+        seg_apellido = self.seg_apellido_b.text
+        insertar(cedula, nombre, seg_nombre, apellido, seg_apellido)
+
+
+class Registro2(Screen):
+    pass
 
 
 class Menu(Screen):
+    manejador2 = ObjectProperty()
     background_image = ObjectProperty(Image(source='data/Build.jpg'))
+
+    def login(self):
+        stat = self.ids.log
+        stat.color = (0, 0, 1, 1)
+        stat.text = "Loggin as " + manejador
+
+    def logout(self):
+        self.manager.get_screen('intro').resetLog()
+        self.manager.get_screen('intro').resetForm()
 
 
 class Materias(Screen):
@@ -52,11 +92,28 @@ class Calendario(Screen):
 
 
 class Perfil(Screen):
+    grid = ObjectProperty()
     background_image = ObjectProperty(Image(source='data/backazul.png'))
+
+    def consultar(self):
+        cedula = manejador
+        consultarperfil(cedula)
+        print(perfil())
+        datos = perfil()
+        for x in datos:
+            print(x)
+            self.grid.add_widget(Label(text=str(x), font_size=10))
+        for x in datos:
+            print(datos.get(x))
+            self.grid.add_widget(Label(text=str(datos.get(x)), font_size=10))
+
+    def Borrar(self):
+        self.grid.clear_widgets()
 
 
 class Eventos(Screen):
     background_image = ObjectProperty(Image(source='data/backazul.png'))
+
 
 kivyfile = Builder.load_file("Interfaz.kv")
 
